cee107c3230002a4d764e5b8f864ad9d7cfc96d7
BraianSins <bsins198@gmail.com>
2022-09-29 15:14:38 -0300

    Initial commit

---
+++ src/JogoDaVelha.java
@@ -0,0 +1,118 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JogoDaVelha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] jogo = new int[3][3];
+        int jogador1 = 1;
+        int jogador2 = 1;
+        int jogadas = 0;
+        int linha, coluna;
+        System.out.println("Digite o nome do jogador 1: ");
+        String nome1 = sc.nextLine();
+        System.out.println("Digite o nome do jogador 2: ");
+        String nome2 = sc.nextLine();
+
+        boolean ganhou = false;
+        while (ganhou == false) {
+            System.out.println("Vez do jogador 1");
+            System.out.println("Digite a linha: ");
+            linha = sc.nextInt();
+            System.out.println("Digite a coluna: ");
+            coluna = sc.nextInt();
+            if (jogo[linha][coluna] == 0) {
+                jogo[linha][coluna] = jogador1;
+                jogadas++;
+            } else {
+                System.out.println("Jogada inválida");
+            }
+            for (int i = 0; i < jogo.length; i++) {
+                for (int j = 0; j < jogo[i].length; j++) {
+                    System.out.print(jogo[i][j] + "|");
+                }
+                System.out.println();
+            }
+
+            if (jogo[0][0] == 1 && jogo[0][1] == 1 && jogo[0][2] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[1][0] == 1 && jogo[1][1] == 1 && jogo[1][2] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[2][0] == 1 && jogo[2][1] == 1 && jogo[2][2] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[0][0] == 1 && jogo[1][0] == 1 && jogo[2][0] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[0][1] == 1 && jogo[1][1] == 1 && jogo[2][1] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[0][2] == 1 && jogo[1][2] == 1 && jogo[2][2] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[0][0] == 1 && jogo[1][1] == 1 && jogo[2][2] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            } else if (jogo[0][2] == 1 && jogo[1][1] == 1 && jogo[2][0] == 1) {
+                ganhou = true;
+                System.out.println("O jogador 1 ganhou!");
+            }
+            if (jogadas == 9) {
+                ganhou = true;
+                System.out.println("Deu velha!");
+            }
+
+            if (ganhou == false) {
+                System.out.println("Vez do jogador 2");
+                System.out.println("Digite a linha: ");
+                linha = sc.nextInt();
+                System.out.println("Digite a coluna: ");
+                coluna = sc.nextInt();
+                if (jogo[linha][coluna] == 0) {
+                    jogo[linha][coluna] = jogador2;
+                    jogadas++;
+                } else {
+                    System.out.println("Jogada inválida");
+                }
+                for (int i = 0; i < jogo.length; i++) {
+                    for (int j = 0; j < jogo[i].length; j++) {
+                        System.out.print(jogo[i][j] + "|");
+                    }
+                    System.out.println();
+                }
+                if (jogo[0][0] == 2 && jogo[0][1] == 2 && jogo[0][2] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[1][0] == 2 && jogo[1][1] == 2 && jogo[1][2] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[2][0] == 2 && jogo[2][1] == 2 && jogo[2][2] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[0][0] == 2 && jogo[1][0] == 2 && jogo[2][0] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[0][1] == 2 && jogo[1][1] == 2 && jogo[2][1] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[0][2] == 2 && jogo[1][2] == 2 && jogo[2][2] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[0][0] == 2 && jogo[1][1] == 2 && jogo[2][2] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                } else if (jogo[0][2] == 2 && jogo[1][1] == 2 && jogo[2][0] == 2) {
+                    ganhou = true;
+                    System.out.println("O jogador 2 ganhou!");
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
