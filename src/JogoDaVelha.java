2e75b69bce7e63a96a8be133ec9b24ec7ad356e1
anatsenger <anathaissenger@hotmail.com>
2022-10-02 15:01:52 -0300

    Arrumei o deu velhaa, falta tratar os erros agora

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -11,6 +11,7 @@ public class JogoDaVelha {
         String tab = Tabuleiro();
         Scanner leitor = new Scanner(System.in);
         char[] arrayTab;
+        int n = 0;
 
 
 
@@ -25,7 +26,7 @@ public class JogoDaVelha {
 
 
         while (!ganhou) {
-
+            n = n + 2;
             for (int i = 0; i < jogadores.length ; i++) {
                 String jogador = jogadores[i];
                 char simbolo = simbolos[i];
@@ -33,8 +34,21 @@ public class JogoDaVelha {
                 posicao = Jogador.getJogadas(leitor);
                 tab = MudaTabuleiro(tab, posicao, simbolo);
                 arrayTab = tabToArray(tab);
-                ganhou = Ganhou(arrayTab, jogador);
+                ganhou = Ganhou(arrayTab, jogador, tab);
                     if (ganhou == true){
+                        System.out.println("Quer jogar outra partida? s/n");
+                        char resposta = leitor.next().charAt(0);
+                            if(resposta == 's'){
+                                System.out.println("|---------------Iniciando outra partida--------------|");
+                                tab = Tabuleiro();
+                            } else {
+                                System.out.println("|---------------Encerrando Jogo--------------|");
+                                break;
+                            }
+                    }
+                    if(n == 10 && i == 0){
+                        System.out.println(tab);
+                        System.out.println("Deu velhaa!!");
                         System.out.println("Quer jogar outra partida? s/n");
                         char resposta = leitor.next().charAt(0);
                             if(resposta == 's'){
@@ -94,23 +108,30 @@ public class JogoDaVelha {
         return lista;
     }
 
-    private static boolean Ganhou (char[] tabJogadas, String jogador){
+    private static boolean Ganhou(char[] tabJogadas, String jogador, String tab){
         boolean ganhou;
-        if (tabJogadas[0] == tabJogadas [1] && tabJogadas [1] == tabJogadas[2] ||
-                tabJogadas[3] == tabJogadas [4] && tabJogadas [4] == tabJogadas[5] ||
-                tabJogadas[6] == tabJogadas [7] && tabJogadas [7] == tabJogadas[8] ||
-                tabJogadas[0] == tabJogadas [3] && tabJogadas [3] == tabJogadas[6] ||
-                tabJogadas[1] == tabJogadas [4] && tabJogadas [4] == tabJogadas[7] ||
-                tabJogadas[2] == tabJogadas [5] && tabJogadas [5] == tabJogadas[8] ||
-                tabJogadas[0] == tabJogadas [4] && tabJogadas [4] == tabJogadas[8] ||
-                tabJogadas[2] == tabJogadas [4] && tabJogadas [4] == tabJogadas[6]){
-            System.out.println("Parabéeennss " +jogador+ " vc ganhou");
-            ganhou = true;
-        } else {
-            ganhou = false;
-        }
+            if (tabJogadas[0] == tabJogadas [1] && tabJogadas [1] == tabJogadas[2] ||
+                    tabJogadas[3] == tabJogadas [4] && tabJogadas [4] == tabJogadas[5] ||
+                    tabJogadas[6] == tabJogadas [7] && tabJogadas [7] == tabJogadas[8] ||
+                    tabJogadas[0] == tabJogadas [3] && tabJogadas [3] == tabJogadas[6] ||
+                    tabJogadas[1] == tabJogadas [4] && tabJogadas [4] == tabJogadas[7] ||
+                    tabJogadas[2] == tabJogadas [5] && tabJogadas [5] == tabJogadas[8] ||
+                    tabJogadas[0] == tabJogadas [4] && tabJogadas [4] == tabJogadas[8] ||
+                    tabJogadas[2] == tabJogadas [4] && tabJogadas [4] == tabJogadas[6]){
+                System.out.println(tab);
+                System.out.println("Parabéeennss " + jogador + " vc ganhou");
+                ganhou = true;
+            } else {
+                ganhou = false;
+            }
         return ganhou;
     }
+
+//    private static boolean DeuVelha(int numeroJogadas, boolean ganhou){
+//        if (numeroJogadas.length > 9 && ganhou == false) {
+//            System.out.println("Deu velhaa!!");
+//        }
+//    }
 
     private static char[] tabToArray(String tab){
         char[] tabuleiro = tab.toCharArray();
